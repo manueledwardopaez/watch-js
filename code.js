2c791ef207ddced86b6d5c1ee217deb30257dd2d
ManuelEdwardoPaez <manueledwardopaez@gmail.com>
2021-02-20 21:32:35 -0400

    Add the am or pm.

--- code.js
+++ code.js
@@ -1,8 +1,18 @@
 function watch() {
     var fecha = new Date();
-    var hora = fecha.getHours() - 12;
+    var hora = fecha.getHours();
     var minutos = fecha.getMinutes();
     var segundos = fecha.getSeconds();
+
+    if (hora >= 13) {
+        var mer = 'PM';
+    } else {
+        mer = 'AM'
+    }
+
+    if (hora >= 13) {
+        hora = hora - 12;
+    }
 
     var h = document.getElementById('h');
     h.innerHTML = hora;
@@ -10,6 +20,8 @@ function watch() {
     m.innerHTML = minutos;
     var s = document.getElementById('s');
     s.innerHTML = segundos;
+    var meridiano = document.getElementById('meridian');
+    meridian.innerHTML = mer;
 }
 
 
